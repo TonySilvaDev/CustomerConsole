3802a616eb50135485b9396da6aeceefb57a69d2
Antonio Silva <antonio.silvamnrl@gmail.com>
2024-09-12 16:24:59 -0600

    4. Implement the Interface in FileCustomerDataProvider

---
+++ CustomerConsole/Repository/FileCustomerDataProvider.cs
@@ -0,0 +1,32 @@
+﻿using CustomerConsole.Repository.IRepository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomerConsole.Repository
+{
+    public class FileCustomerDataProvider : ICustomerDataProvider
+    {
+        public List<Customer> GetCustomerList()
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Customer> GetCustomerListByAgeRange(int startAge, int endAge)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void SaveCustomer(Customer customer)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void SaveCustomerList(List<Customer> customerList)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
